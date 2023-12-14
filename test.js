5060ad49a8f42ea732234d6c18f636a182ce7a7e
davidedemelas <davidedemelas.nba@gmail.com>
2023-12-14 14:43:12 +0100

    risoluzione problema div, miglioramento css

--- test.js
+++ test.js
@@ -3,33 +3,34 @@ let inserimento_drink = document.getElementById("drink_name")
 let button = document.getElementById("pulsante")
 
 async function getData() {
+
+  result.innerHTML = ""
+
   const nome_drink = inserimento_drink.value
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nome_drink}`;
   const response = await fetch(url);
   const data = await response.json()
   const drinks = await data.drinks
 
-
-
-
-
-
-  
-
-  //stampare un elenco di quei drink nella console
-  /*
   drinks.forEach(drink => {
     result.innerHTML += `
-      <div class="single-drink">
-        <img src="${drink.strDrinkThumb}" width=100>
-        <p> ${drink.strDrink} </p>
-      </div>
+    <div class="drink_container">
+        <div class="drink_image"> 
+            <img width=100 src="${drink.strDrinkThumb}">
+        </div>
+        <div class="description"> 
+            <p class="title"> ${drink.strDrink} </p>
+            <p class="preparation"> </p>
+        </div>
+    </div>
     `
   });
-  */
-
-  result.innerHTML = `ciao uomo`
 }
+
+button.addEventListener("click", function(){
+  getData()
+})
+
 window.addEventListener("load", function(){
-  result.innerHTML = `ciao fra`
+  getData()
 })
